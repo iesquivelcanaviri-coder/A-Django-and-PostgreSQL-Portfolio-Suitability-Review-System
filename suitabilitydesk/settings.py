0047f73fa959f50ca5314c3877a439f01c36c17a
iesquivelcanaviri-coder <iesquivelcanaviri@gmail.com>
2026-08-12 19:37:43 +0200

    v5

--- suitabilitydesk/settings.py
+++ suitabilitydesk/settings.py
@@ -174,7 +174,6 @@ EMAIL_BACKEND = os.environ.get(
     "EMAIL_BACKEND",
     "django.core.mail.backends.console.EmailBackend"
 )
-
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
